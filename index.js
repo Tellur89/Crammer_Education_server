6999bfba5057c566d55190ec8ad5d117e88cf3dc
Tom Burzynski <tellur.beryl@gmail.com>
2023-05-30 11:24:52 +0100

    add userRoutes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ mongoose
 	.connect(process.env.DB_URL)
 	.then(() => {
 		app.listen(PORT || 3000, () => {
-			console.log(`Server is running on port: ${PORT}`);
+			console.log(`Connected to the DB & Server is running on port: ${PORT}`);
 		});
 	})
 	.catch((error) => {
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,9 @@
 const { Router } = require('express');
-const userController = require('../controllers/userController');
+const { loginUser, signupUser } = require('../controllers/userController');
 
 const router = Router();
 
-router.route('/').get(userController.index).post(userController.create);
-
-router.route('/:id').get(userController.show).delete(userController.destroy);
+router.route('/login').post(loginUser);
+router.route('/signup').post(signupUser);
 
 module.exports = router;

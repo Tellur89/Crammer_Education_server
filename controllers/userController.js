04723423464f31a3f9e209f508b5d68d5aca424f
Tom Burzynski <tellur.beryl@gmail.com>
2023-05-30 11:37:47 +0100

    fix db authorization bug

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 const User = require('../models/User');
-// TODO: import token
+// TODO: import token package
 
 // TODO: create createToken fn using _id, JWT_SECRET and expiration
 
@@ -29,54 +29,3 @@ const signupUser = async (req, res) => {
 };
 
 module.exports = { loginUser, signupUser };
-
-// const bcrypt = require('bcrypt');
-// require('dotenv').config();
-// const mongoose = require('mongoose');
-// const SALT = process.env.BCRYPT_SALT_ROUNDS;
-
-// const index = async (req, res) => {
-// 	try {
-// 		const users = await User.getAll();
-// 		res.status(200).json({ users });
-// 	} catch (error) {
-// 		res.status(500).json({ error: error, massage: "Can't connect to server" });
-// 	}
-// };
-
-// const show = async (req, res) => {
-// 	try {
-// 		const { id } = req.params;
-// 		const user = await User.getOneById(id);
-// 	} catch (error) {
-// 		res.status(404).json({ error: error, message: "Can't find the user by given ID" });
-// 	}
-// };
-
-// const create = async (req, res) => {
-// 	try {
-// 		const data = req.body;
-// 		const salt = await bcrypt.genSalt(parseInt(SALT));
-// 		data['password'] = await bcrypt.hash(data['password'], salt);
-// 		const user = await User.createUser(data);
-// 		res.status(201).json({ message: 'User created successfully', user });
-// 	} catch (error) {
-// 		res.status(404);
-// 	}
-// };
-
-// const destroy = async (req, res) => {
-// 	try {
-// 		const { username } = req.params;
-// 		const user = await User.getOneByUsername(username);
-// 		const res = await user.deleteUser();
-// 		res.status(204).json({ message: 'User removed successfully', res });
-// 	} catch (error) {
-// 		res.status(404).json({
-// 			message: 'Could not remove the user from database',
-// 			error,
-// 		});
-// 	}
-// };
-
-// module.exports = { index, show, create, destroy };
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = require('./app');
 const PORT = process.env.PORT;
 
 mongoose
-	.connect(process.env.DB_URL)
+	.connect(process.env.DB_URI)
 	.then(() => {
 		app.listen(PORT || 3000, () => {
 			console.log(`Connected to the DB & Server is running on port: ${PORT}`);

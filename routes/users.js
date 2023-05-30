37dbbcc0708a327d496d1eccd835a38e224c873b
AjayaBudha <ajaya.budhathoki@hotmail.com>
2023-05-30 12:15:03 +0100

    add CRUD to controllers, create Flashcard model

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,12 @@
-const { Router } = require('express');
-const userController = require('../controllers/userController');
+const { Router } = require("express");
+const userController = require("../controllers/userController");
 
 const router = Router();
 
-router.route('/').get(userController.index).post(userController.create);
+router.route("/").get(userController.index).post(userController.create);
 
-router.route('/:id').get(userController.show).delete(userController.destroy);
+router.route("/:id").get(userController.show).delete(userController.destroy);
+
+router.route("/:id/");
 
 module.exports = router;

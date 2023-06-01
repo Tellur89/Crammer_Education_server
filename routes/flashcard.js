622dc51e53411b6a59b08afae3583519c9145602
Tom Burzynski <tellur.beryl@gmail.com>
2023-06-01 23:20:30 +0100

    fix typos

--- routes/flashcard.js
+++ routes/flashcard.js
@@ -1,17 +1,12 @@
-const { Router } = require("express");
-const cardController = require("../controllers/flashcardController");
-const auth = require("../middleware/auth");
+const { Router } = require('express');
+const cardController = require('../controllers/flashcardController');
+const auth = require('../middleware/auth');
 
 const router = Router();
 
 router.use(auth);
-router
-  .route("/")
-  .get(cardController.getFlashCards)
-  .post(cardController.createFlashCard)
-  .patch(cardController.updateFlashcard)
-  .delete(cardController.deleteFlashcard);
+router.route('/').get(cardController.getFlashCards).post(cardController.createFlashCard).patch(cardController.updateFlashcard);
 
-router.route("/:id").delete(cardController.deleteFlashcard);
+router.route('/:id').delete(cardController.deleteFlashcard);
 
 module.exports = router;

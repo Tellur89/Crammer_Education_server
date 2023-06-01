feabfbe038ba35adf8dd8765e50d1c3090e2d994
Tom Burzynski <tellur.beryl@gmail.com>
2023-06-01 12:13:06 +0100

    minors

--- __test__/auth.test.js
+++ __test__/auth.test.js
@@ -3,30 +3,3 @@ const auth = require('../middleware/auth');
 describe('Authorization', () => {
 	describe();
 });
-
-// const jwt = require('jsonwebtoken');
-// const request = require('supertest');
-// const User = require('../models/User');
-// const app = require('../app');
-
-// describe('Auth Middleware', () => {
-// 	let user;
-// 	let token;
-
-// 	beforeEach(async () => {
-// 		user = await User.signup('testUser', 'test@user.com', 'Password123!');
-// 		token = jwt.sign(req.headers.split(' ')[1], 'aisujhdiausfhiausfh');
-// 	});
-
-// 	afterEach(async () => {
-// 		await User.deleteMany({});
-// 	});
-
-// 	it('should return 401 if no authorization header is provided', async () => {
-// 		const response = await request.get('/flashcards');
-
-// 		expect(response.status).toBe(401);
-// 		expect(response.body.error).toBe('You are not authorised');
-// 	});
-
-// });
